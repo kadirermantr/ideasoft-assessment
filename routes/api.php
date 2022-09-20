f8dea7dc49c96515974608867ce8e0ba0df99e85
Kadir Erman <kadirermantr@gmail.com>
2022-09-20 12:20:01 +0300

    [feat] Add new routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\DiscountController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +21,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::resource('customer', CustomerController::class);
+Route::resource('product', ProductController::class);
+Route::resource('order', OrderController::class);
